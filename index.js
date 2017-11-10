ee911a4995b634f6d6ebd310a010a4b058918c7f
Max han <mwellh84@gmail.com>
2017-11-10 13:10:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function overwriteBestCustomer(){
   bestCustomer = 'maybe bob'
 }
 
-var leastFavoriteCustomer = 'sam'
+leastFavoriteCustomer = 'sam'
 
 changeLeastFavoriteCustomer(){
   leastFavoriteCustomer = 'julie'
